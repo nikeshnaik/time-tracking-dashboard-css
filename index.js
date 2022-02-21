c7f2b82d4c0b9931c9edc4379c1907dd3860a494
nikeshnaik <nikeshnaik.dev@gmail.com>
2022-02-21 20:12:57 +0530

    feat(final solution): Refactored Javascript code and added background svgs

--- index.js
+++ index.js
@@ -103,62 +103,38 @@ var data = [
     }
 ]
 
-console.log(data)
 
 const timeframes = ["daily", "weekly", "monthly"]
-myStorage = window.localStorage;
 
 
-// function section_template_mapper(section) {
-//     let filter = myStorage.getItem('filter')
-//     return section_template_filler(section, filter)
-// }
-
-
-// function section_template_filler(section, filter) {
-
-//     title = section.title
-//     css_class = section.title.toLowerCase().split(" ").join("_")
-//     current = section["timeframes"][filter]["current"]
-//     previous = section["timeframes"][filter]["previous"]
-
-//     var template = `
-//         <div class="${css_class}_section">
-//         <div class="eclipse_background"></div>
-//         <div class="header">
-//         <span id="title">${title}</span>
-//         <span id="btn">Button</span>
-//         </div>
-//             <div class="header-text">
-//                 <p id="unit-value">${current}hrs</p>
-//                 <p id="previous-unit-value">Previous - ${previous}hrs</p>
-//             </div>
-//         </div> `
-
-//     return template
-
-// }
-
-
-
-document.getElementsByClassName("time_frame")[0].innerHTML = timeframes.map((element) => {
+// creates anchor tag for each timeframe and calls event listener setup.
+timeframes.forEach(element => {
 
     id_tag = element
     text = element
 
-    var template = `<a id="${id_tag}" href="#">${text}</a>`
+    a_tag = document.createElement("a")
+    a_tag.id = id_tag
+    a_tag.textContent = text
 
-    return template
+    document.getElementsByClassName("time_frame")[0].appendChild(a_tag)
+    events_to_anchors(element)
+});
 
-}).join(" ")
 
-
+//// Setups event listener for each timeframe, we add more to make dynamic
 
 function events_to_anchors(timeframe) {
 
     document.getElementById(timeframe).addEventListener("click", () => {
-        console.log(timeframe)
-        template_tag = document.getElementById("grids")
+
+        localStorage.setItem("timeframe", timeframe)
+
+        timeframes.forEach(element => {
+            document.getElementById(element).style.color = "white"
+        });
+
+        document.getElementById(timeframe).style.color = "var(--neutral-Desaturated-blue)"
 
 
         data.forEach(section => {
@@ -166,10 +142,6 @@ function events_to_anchors(timeframe) {
             css_class = section.title.toLowerCase().split(" ").join("_") + "_section"
             current = section["timeframes"][timeframe]["current"]
             previous = section["timeframes"][timeframe]["previous"]
-            // console.log(current)
-            // console.log(previous)
-            console.log(css_class)
-            // console.log(title)
 
             section_div = create_grid_item(css_class, title, current, previous)
             document.getElementsByTagName("main")[0].append(section_div)
@@ -178,15 +150,10 @@ function events_to_anchors(timeframe) {
 
     })
 
-    return null
 }
 
 
-
-events_to_anchors("daily")
-events_to_anchors("weekly")
-events_to_anchors("monthly")
-
+// Builds the sections, since using innerhtml reconstruct the dom and event listeners are lost.
 
 function create_grid_item(css_class, title, current, previous) {
     // <div class="${css_class}_section">
@@ -202,43 +169,56 @@ function create_grid_item(css_class, title, current, previous) {
     // </div> `
 
     section_div = document.createElement("div")
-    section_div.classname = css_class
+    section_div.className = css_class
     eclipse_div = document.createElement("div")
-    eclipse_div.classname = "eclipse_background"
+    eclipse_div.className = "eclipse_background"
     section_div.appendChild(eclipse_div)
 
-    header_div = document.createElement("header")
+    header_div = document.createElement("div")
+    header_div.className = "header"
     span_title = document.createElement("span")
     span_title.id = "title"
     span_title.textContent = title
-    span_btn = document.createElement("span")
-    span_btn.id = "btn"
-    span_btn.textContent = "button"
 
+    span_btn = document.createElement("span")
+    span_btn.className = "img_span"
+    btn_img = document.createElement("img")
+    btn_img.id = "btn_img"
+    btn_img.setAttribute("src", "images/icon-ellipsis.svg")
+    span_btn.appendChild(btn_img)
 
     header_div.appendChild(span_title)
     header_div.appendChild(span_btn)
     section_div.appendChild(header_div)
 
     header_text_div = document.createElement("div")
-    header_text_div.classname = "header-text"
+    header_text_div.className = "header-text"
     p_current = document.createElement("p")
     p_current.id = "unit-value"
-    p_current.textContent = current
+    p_current.textContent = current + "hrs"
     p_previous = document.createElement("p")
     p_previous.id = "previous-unit-value"
-    p_previous.textContent = previous
+    p_previous.textContent = "Previous - " + previous + "hrs"
 
     header_text_div.appendChild(p_current)
     header_text_div.appendChild(p_previous)
     section_div.appendChild(header_text_div)
 
-    console.log(section_div)
+    // console.log(section_div)
     return section_div
 
 }
 
 
-// create_grid_item("work_section", "Work", "1000", "2112")
+
+// Init by clicking on event listener and storing the state in local storage
+if (!localStorage.getItem("timeframe")) {
+    localStorage.setItem("timeframe", "monthly")
+}
+
+document.getElementById(localStorage.getItem("timeframe")).click()
 
 
+
+/// New things learned is, the event listener are removed if innerhtml is used.
+/// Cannot toggle multiple classes at once, need looping
